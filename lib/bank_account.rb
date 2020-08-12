7486c550a69389d66e676ef671e49af5335a46d3
Nerly Ton <nerlyton@gmail.com>
2020-08-12 03:15:15 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
   
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
   attr_reader :bank_account
   
   def initialize(name)
   @bank_account = bank_account
   @name = name 
   @balance = 1000
+  @status = "open"
   end
 end
