fe4e661f86965bcd419a3ae8f066b9ff9dc72c0a
Nerly Ton <nerlyton@gmail.com>
2020-08-12 03:30:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
   
-  attr_accessor :transfer, :sender, :receiver
+  attr_accessor :transfer, :sender, :receiver, :status
   
   def initialize(transfer, sender, receiver)
     @transfer = transfer 
     @sender = sender
     @receiver = receiver
+    @status = "pending"
   end
   
 end
